b19544da304fbc023e041d2ad4bab5c1d83e9760
jordan3900 <m.iordanize@gmail.com>
2018-01-22 22:52:25 +0200

    Add project files.

---
+++ Blogen/Startup.cs
@@ -0,0 +1,19 @@
+﻿using Blogen.Migrations;
+using Blogen.Models;
+using Microsoft.Owin;
+using Owin;
+using System.Data.Entity;
+
+[assembly: OwinStartupAttribute(typeof(Blogen.Startup))]
+namespace Blogen
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            Database.SetInitializer(new MigrateDatabaseToLatestVersion<BlogDbContext, Configuration>());
+
+            ConfigureAuth(app);
+        }
+    }
+}
